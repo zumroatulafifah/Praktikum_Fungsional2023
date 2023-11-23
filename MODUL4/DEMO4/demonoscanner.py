b1be3be65c4b99b023cd99016f9b48614bde6c60
afifah <zumroatulafifah484@gmail.com>
2023-11-24 06:34:56 +0700

    modul 4 with abstrac class, abstrac method, inheritence, and magic method

---
+++ MODUL4/DEMO4/demonoscanner.py
@@ -0,0 +1,111 @@
+# import math
+
+# def point(x, y):
+#     return x, y
+
+# def line_equation_of(p1, M):
+#     x1, y1 = p1
+#     C = y1 - M * x1
+#     return f"y = {M:.2f}x + {C:.2f}"
+
+# # Decorator untuk transformasi gabungan
+# def combined_transform(func_list):
+#     def decorator(p):
+#         for transform_func in func_list:
+#             p = transform_func(p)
+#         return p
+#     return decorator
+
+# # Translasi
+# def translate(p):
+#     x, y = p
+#     return point(x + 9, y + 9)
+
+# # Rotasi
+# def rotate(p):
+#     x, y = p
+#     x_rotated = x * math.cos(math.radians(60)) - y * math.sin(math.radians(60))
+#     y_rotated = x * math.sin(math.radians(60)) + y * math.cos(math.radians(60))
+#     return point(x_rotated, y_rotated)
+
+# # Perbesaran skala
+# def scale(p):
+#     x, y = p
+#     return point(x * 9, y * 0)
+
+# # Titik awal
+# A = point(0, 9)
+
+# # Gradien
+# M = 0
+
+# # List transformasi
+# func_list = [translate, rotate, scale]
+
+# # Transformasi gabungan
+# A_transformed = combined_transform(func_list)(A)
+
+# # Menentukan persamaan garis setelah transformasi
+# equation_transformed = line_equation_of(A_transformed, M)
+
+# print("Persamaan garis yang melalui titik (0,9) dan bergradien 0:")
+# print(line_equation_of(A, M))
+# print("\nPersamaan garis baru setelah transformasi:")
+# print(equation_transformed)
+
+import math
+
+def point(x, y):
+    return x, y
+
+def line_equation_of(p1, M):
+    x1, y1 = p1
+    C = y1 - M * x1
+    return f"y = {M:.2f}x + {C:.2f}"
+
+# Decorator untuk transformasi gabungan
+def combined_transform(*func_list):
+    def decorator(p):
+        for transform_func in func_list:
+            p = transform_func(p)
+        return p
+    return decorator
+
+# Fungsi decorator dengan simbol "@"
+@combined_transform
+def translate(p):
+    x, y = p
+    return point(x + 9, y + 9)
+
+@combined_transform
+def rotate(p):
+    x, y = p
+    x_rotated = x * math.cos(math.radians(60)) - y * math.sin(math.radians(60))
+    y_rotated = x * math.sin(math.radians(60)) + y * math.cos(math.radians(60))
+    return point(x_rotated, y_rotated)
+
+@combined_transform
+def scale(p):
+    x, y = p
+    return point(x * 9, y * 0)  # Mengubah faktor skala y menjadi 9
+
+# Titik awal
+A = point(0, 9)
+
+# Gradien
+M = 0
+
+# Menggunakan decorator pada titik A
+# A_transformed = translate(rotate(scale(A)))
+# Menggunakan decorator pada titik A
+A_transformed = scale(rotate(translate(A)))
+
+
+
+# Menentukan persamaan garis setelah transformasi
+equation_transformed = line_equation_of(A_transformed, M)
+
+print("Persamaan garis yang melalui titik (0,9) dan bergradien 0:")
+print(line_equation_of(A, M))
+print("\nPersamaan garis baru setelah transformasi:")
+print(equation_transformed)
